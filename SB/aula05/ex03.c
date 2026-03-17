e3f86aa9183b4e543445e8827dd4c4a0f154db8b
Alan007BR <alanoliveira123bit@gmail.com>
2026-03-17 09:37:43 -0300

    Fix formatting and correct syntax error in flip2 function

--- SB/aula05/ex03.c
+++ SB/aula05/ex03.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 
-
-
 void flip(int *value, int bit) {
-  *value ^= (1 << bit);
+    *value ^= (1 << bit);
 
-//   ^=' is o operador XOR, que inverte o bit na posição especificada. 
-//   O operador de deslocamento à esquerda (<<) é usado para criar uma máscara com um 
-//   único bit definido na posição desejada.'
+    //   ^=' is o operador XOR, que inverte o bit na posição especificada. 
+    //   O operador de deslocamento à esquerda (<<) é usado para criar uma máscara com um 
+    //   único bit definido na posição desejada.'
 }
 
-
+void flip2(int *value, int bit) {
+    if(*value & (1 << bit)b==0) {
+        *value = *value | (1 << bit); // Se o bit estiver definido, desativa-o usando OR com a máscara.
+    } else {
+        *value = *value & ~(1 << bit); // Se o bit não estiver definido, ativa-o usando AND com a negação da máscara.
+    }
+}
 
 int main() {
   int value;
